ad99a6148630564f2b393f3a94a2e64f2dc38eb0
Aditya Mishra <mishra.aditya1352@gmail.com>
2023-07-06 19:54:44 +0530

    Text Utilities

---
+++ src/components/TextForm.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function TextForm() {
+  return (
+    <>
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlTextarea1" className="form-label">
+          Example textarea
+        </label>
+        <textarea
+          className="form-control"
+          id="exampleFormControlTextarea1"
+          rows="3"
+        ></textarea>
+      </div>
+    </>
+  );
+}
