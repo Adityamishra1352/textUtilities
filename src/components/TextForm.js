23cf5eba8805f0d72bb4ce8c38e20e20ef9c1c9f
Aditya Mishra <mishra.aditya1352@gmail.com>
2023-07-06 21:48:54 +0530

    Added more features

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,33 +1,74 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 export default function TextForm(props) {
-    const handleClick=()=>{
-        console.log("Uppercase was clicked");
-        let newText=text.toUpperCase();
-        setText(newText);
+  const handleClick = () => {
+    console.log("Uppercase was clicked");
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+  const handleLowerCase = (event) => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+  const capitalized=()=>{
+    var arr = text.split(" ")
+    var c = arr.length
+    var temp = ""
+    while(c!==0){
+        temp = arr[c-1].charAt(0).toUpperCase()+arr[c-1].substring(1).toLowerCase()+" "+temp
+        c--;
     }
-    const handleOnChange=(event)=>{
-        setText(event.target.value)
-    }
-    const handleLowerCase=(event)=>{
-        let newText=text.toLowerCase();
-        setText(newText);
-    }
-    const [text,setText]=useState('Enter text here'); 
-    // setText("hii i am aditya");
-    return (
+
+    setText(temp)
+}
+  const speak = () => {
+    let msg = new SpeechSynthesisUtterance();
+    msg.text = text;
+    window.speechSynthesis.speak(msg);
+  };
+  const handleClearText = (event) => {
+    setText("");
+  };
+  const [text, setText] = useState("");
+  // setText("hii i am aditya");
+  return (
     <>
-    <h1>
-        {props.heading}
-    </h1>
-      <div className="mb-3">
+      <div className="mb-3 container my-2">
+        <h1>{props.heading}</h1>
         <textarea
           className="form-control"
-          id="exampleFormControlTextarea1" value={text} onChange={handleOnChange}
+          id="exampleFormControlTextarea1"
+          value={text}
+          onChange={handleOnChange}
           rows="8"
         ></textarea>
-        <button className="btn btn-primary my-2" onClick={handleClick}>Convert to Upper Case</button>
-        <button className="btn btn-primary my-2" onClick={handleLowerCase}>Convert to Lower Case</button>
+        <button className="btn btn-primary my-2 mx-1" onClick={handleClick}>
+          Convert to Upper Case
+        </button>
+        <button className="btn btn-primary my-2 mx-1" onClick={handleLowerCase}>
+          Convert to Lower Case
+        </button>
+        <button className="btn btn-primary my-2 mx-1" onClick={speak}>
+          Speak
+        </button>
+        <button className="btn btn-primary my-2 mx-1" onClick={capitalized}>
+          Capitalize
+        </button>
+        <button className="btn btn-danger my-2 mx-1" onClick={handleClearText}>
+          Clear text
+        </button>
+      </div>
+      <div className="container my-2">
+        <h2>Your text summary</h2>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minuutes Read</p>
+        <h3>Preview</h3>
+        <p>{text}</p>
       </div>
     </>
   );
