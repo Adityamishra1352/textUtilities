9d19f6db58ce5251ce43b850bd78d0087c186963
Aditya Mishra <mishra.aditya1352@gmail.com>
2023-07-06 20:39:59 +0530

    LowerCase

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -9,6 +9,10 @@ export default function TextForm(props) {
     const handleOnChange=(event)=>{
         setText(event.target.value)
     }
+    const handleLowerCase=(event)=>{
+        let newText=text.toLowerCase();
+        setText(newText);
+    }
     const [text,setText]=useState('Enter text here'); 
     // setText("hii i am aditya");
     return (
@@ -23,6 +27,7 @@ export default function TextForm(props) {
           rows="8"
         ></textarea>
         <button className="btn btn-primary my-2" onClick={handleClick}>Convert to Upper Case</button>
+        <button className="btn btn-primary my-2" onClick={handleLowerCase}>Convert to Lower Case</button>
       </div>
     </>
   );
