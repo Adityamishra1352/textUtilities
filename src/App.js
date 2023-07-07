ec919ed96b66a6c130c4b134a73754a6e3dc202b
Aditya Mishra <mishra.aditya1352@gmail.com>
2023-07-07 22:48:57 +0530

    alert

--- src/App.js
+++ src/App.js
@@ -3,21 +3,32 @@ import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 function App() {
   const[darkMode,setDarkMode]=useState('light');
+  const[alert,setAlert]=useState(null);
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+  }
   const toggleMode=()=>{
     if(darkMode==='light'){
       setDarkMode("dark");
       document.body.style.backgroundColor="grey";
+      showAlert("Dark Mode has been enabled", "success");
     }
     else{
       setDarkMode("light");
       document.body.style.backgroundColor="white";
+      showAlert("Light Mode has been enabled","success");
     }
   }
   return (
     <>
 <Navbar title="textUtils" about="About TextUtils" mode={darkMode} toggleMode={toggleMode}></Navbar>
+<Alert alert={alert}></Alert>
 <div className="container">
 <TextForm heading="Enter the text to analyze" mode={darkMode}></TextForm>
 {/* <About></About> */}
